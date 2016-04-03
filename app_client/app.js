432e26d0e70edc0f3d77de7a005eb49e021fedd0
Zheondre <Angel.Calcano@philips.com>
2016-04-03 18:44:18 -0400

    adding files from chapter 11

---
+++ app_client/app.js
@@ -0,0 +1,59 @@
+/*
+	File:  app_client/app.js
+	COMP 4620 Project: Fete
+	Angel Calcano, UMass Lowell Computer Science, Angel_Calcano@cs.uml.edu
+	Copyright (c) 2016 by Angel Calcano.  All rights reserved.  May be freely 
+	copied or excerpted for educational purposes with credit to the author.
+	created by AC on 4/2/2016.
+*/
+//https://github.com/simonholmes/getting-MEAN/blob/chapter-09/app_client/app.js
+(function () {
+
+  angular.module('feteApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
+
+  function config ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '/home/home.view.html',
+        controller: 'homeCtrl',
+        controllerAs: 'vm'
+      })
+      .when('/about', {
+        templateUrl: '/common/views/genericText.view.html',
+        controller: 'aboutCtrl',
+        controllerAs: 'vm'
+      })
+      .when('/location/:locationid', {
+        templateUrl: '/locationDetail/locationDetail.view.html',
+        controller: 'locationDetailCtrl',
+        controllerAs: 'vm'
+      })
+      .when('/signup', {
+        templateUrl: '/auth/register/register.view.html',
+        controller: 'registerCtrl',
+        controllerAs: 'vm'
+      })
+      .when('/log', {
+        templateUrl: '/views/login.jade',
+        controller: 'loginCtrl',
+        controllerAs: 'vm'
+      })
+      .otherwise({redirectTo: '/'});
+     /* 
+     .when('/Profile', {
+        templateUrl: '/app_server/views/profile.jade',
+        controller: 'profileCtrl',
+        controllerAs: 'vm'
+      })
+     
+     
+     */ 
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true);
+  }
+
+  angular
+    .module('feteApp')
+    .config(['$routeProvider', '$locationProvider', config]);
+
+})();`
---
+++ app_client/auth/register/register.controller.js
@@ -0,0 +1,50 @@
+(function () {
+
+  angular
+    .module('feteApp')
+    .controller('registerCtrl', registerCtrl);
+
+  registerCtrl.$inject = ['$location','authentication'];
+  function registerCtrl($location, authentication) {
+    var vm = this;
+
+    vm.pageHeader = {
+      title: 'Create a new Loc8r account'
+    };
+
+    vm.credentials = {
+      name : "",
+      email : "",
+      password : "",
+	  confirmpw: ""
+    };
+
+    vm.returnPage = $location.search().page || '/login'; // go to login 
+
+    vm.onSubmit = function () {
+      vm.formError = "";
+	  // put if for confirmpw
+      if (!vm.credentials.name || !vm.credentials.email || !vm.credentials.password) {
+        vm.formError = "All fields required, please try again";
+        return false;
+      } else {
+        vm.doRegister();
+      }
+    };
+
+    vm.doRegister = function() {
+      vm.formError = "";
+      authentication
+        .register(vm.credentials)
+        .error(function(err){
+          vm.formError = err;
+        })
+        .then(function(){
+          $location.search('page', null); 
+          $location.path(vm.returnPage);
+        });
+    };
+
+  }
+
+})();
